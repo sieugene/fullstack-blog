076954a5c0b975b5dfb616c1f2936d70c48f665a
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-11 20:06:55 +0300

    init, add globals scss

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-container>
+    <el-header>
+      <Navigation />
+    </el-header>
+    <el-main><Nuxt /></el-main>
+    <el-footer />
+  </el-container>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
