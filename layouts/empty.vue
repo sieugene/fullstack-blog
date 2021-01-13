2868977e397cccabc4b2280ab6cb0adf9433cd88
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-13 18:37:58 +0300

    login layout

---
+++ layouts/empty.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="empty-layout">
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.empty-layout {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-top: 4rem;
+}
+</style>
---
+++ pages/admin/login.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-card shadow="always" :style="{ width: '500px' }">
+    <el-form
+      :model="controls"
+      :rules="rules"
+      ref="form"
+      @submit.native.prevent="onSubmit"
+    >
+      <h2>Войти в панель администратора</h2>
+      <el-form-item label="Логин" prop="login">
+        <el-input v-model.trim="controls.login" />
+      </el-form-item>
+      <div class="mb2">
+        <el-form-item label="Пароль" prop="password">
+          <el-input type="password" v-model.trim="controls.password" />
+        </el-form-item>
+      </div>
+
+      <el-form-item>
+        <el-button type="primary" round native-type="submit" :loading="loading"
+          >Войти</el-button
+        >
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+<script>
+export default {
+  layout: "empty",
+  data() {
+    return {
+      loading: false,
+      controls: {
+        login: "",
+        password: ""
+      },
+      rules: {
+        login: [
+          {
+            required: true,
+            message: "Введите логин",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "Введите пароль",
+            trigger: "blur"
+          },
+          {
+            min: 6,
+            message: "Пароль должен быть не менее 6 символов",
+            triggre: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit");
+    }
+  }
+};
+</script>
+<style lang="scss" scoped></style>
