707343d80f5b110cbd14bd40827663cbb5d05ff2
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-14 19:53:21 +0300

    list layout, async promise mock data

--- pages/admin/list.vue
+++ pages/admin/list.vue
@@ -1,11 +1,64 @@
 <template>
-  <div>
-    list
-  </div>
+  <el-table :data="posts" style="width: 100%">
+    <el-table-column prop="title" label="Название" />
+
+    <el-table-column label="Дата" width="180">
+      <template slot-scope="{ row: { date } }">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{
+          new Date(date).toLocaleString()
+        }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column prop="views" label="Просмотры">
+      <template slot-scope="{ row: { views } }">
+        <i class="el-icon-view"></i>
+        <span style="margin-left: 10px">{{ views }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="Комментарии" width="180">
+      <template slot-scope="{ row: { comments } }">
+        <i class="el-icon-message"></i>
+        <span style="margin-left: 10px">{{ comments.length }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="Действия">
+      <template slot-scope="{ row }">
+        <el-button
+          circle
+          icon="el-icon-edit"
+          type="primary"
+          @click="open(row._id)"
+        />
+
+        <el-button
+          icon="el-icon-delete"
+          type="danger"
+          circle
+          @click="remove(row._id)"
+        />
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 <script>
 export default {
   layout: "admin",
-  middleware: ["admin-auth"]
+  middleware: ["admin-auth"],
+  async asyncData({ store }) {
+    const posts = await store.dispatch("post/fetchAdmin");
+    return { posts };
+  },
+  methods: {
+    open(id) {
+      console.log("open", id);
+    },
+    remove(id) {
+      console.log("remove", id);
+    }
+  }
 };
 </script>
---
+++ store/post.js
@@ -0,0 +1,24 @@
+export const actions = {
+  async fetchAdmin({}) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          {
+            title: "Post1",
+            date: new Date(),
+            views: 22,
+            comments: [1, 2],
+            _id: Math.random()
+          },
+          {
+            title: "Post2",
+            date: new Date(),
+            views: 22,
+            comments: [1, 2],
+            _id: Math.random()
+          }
+        ]);
+      }, 1);
+    });
+  }
+};
