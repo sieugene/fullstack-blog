df67203db05ada45eb6f9db7c8894a39e3cd9ecd
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-16 19:45:26 +0300

    mongo connect

--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const keys = require("./keys");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose
+  .connect(keys.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch(e => {
+    console.log(e);
+  });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-console.log("MIDDLEWARE API!!!");
 
 module.exports = app;
