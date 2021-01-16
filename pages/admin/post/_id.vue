c06952f0eb07a6104a6c6a4cb79e06aee3afbdba
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-16 17:18:50 +0300

    post page

--- pages/admin/post/_id.vue
+++ pages/admin/post/_id.vue
@@ -1,14 +1,105 @@
 <template>
-  <div>
-    <el-breadcrumb separator="/">
+  <div class="page-wrap">
+    <el-breadcrumb separator="/" class="mb">
       <el-breadcrumb-item to="/admin/list">Посты</el-breadcrumb-item>
-      <el-breadcrumb-item> {{ $route.params.id }}</el-breadcrumb-item>
+      <el-breadcrumb-item> {{ post.title }}</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <el-form
+      :model="controls"
+      :rules="rules"
+      ref="form"
+      @submit.native.prevent="onSubmit"
+    >
+      <!-- <h2>Войти в панель администратора</h2> -->
+      <el-form-item label="Текст в формате .md или .html" prop="text">
+        <el-input
+          type="textarea"
+          resize="none"
+          :rows="10"
+          v-model.trim="controls.text"
+        />
+      </el-form-item>
+      <div class="mb">
+        <small class="mr">
+          <i class="el-icon-time"></i>
+          <span>{{ new Date(post.date).toLocaleString() }}</span>
+        </small>
+        <small>
+          <i class="el-icon-view"></i>
+          <span>{{ post.views }}</span>
+        </small>
+      </div>
+
+      <el-form-item>
+        <el-button type="primary" round native-type="submit" :loading="loading"
+          >Обновить</el-button
+        >
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
 export default {
   layout: "admin",
-  middleware: ["admin-auth"]
+  middleware: ["admin-auth"],
+  head() {
+    return {
+      title: `Пост | ${this.post.title}`
+    };
+  },
+  validate({ params }) {
+    return Boolean(params.id);
+  },
+  async asyncData({ store, params }) {
+    const post = await store.dispatch("post/fetchAdminById", params.id);
+    return { post };
+  },
+  data() {
+    return {
+      loading: false,
+      controls: {
+        text: ""
+      },
+      rules: {
+        text: [
+          {
+            required: true,
+            message: "Текст не должен быть пустым",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.form.validate(async valid => {
+        if (valid) {
+          this.loading = true;
+          try {
+            const formData = {
+              text: this.controls.text,
+              id: this.post._id
+            };
+            await this.$store.dispatch("post/update", formData);
+            this.loading = false;
+            this.$message.success("Пост обновлен");
+          } catch (e) {
+            this.loading = false;
+            this.$message.error(e);
+          }
+        }
+      });
+    }
+  }
 };
 </script>
+<style lang="scss" scoped>
+.page-wrap {
+  width: 600px;
+}
+.mr {
+  margin-right: 2rem;
+}
+</style>
