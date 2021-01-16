24427412f829dfe8ed3e389ef0c828eb4038fa93
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-16 23:09:01 +0300

    passport

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth.routes");
+const passport = require("passport");
+const passportStrategy = require("./middleware/passport-strategy");
 const keys = require("./keys");
 const app = express();
 
@@ -16,6 +18,10 @@ mongoose
   .catch(e => {
     console.log(e);
   });
+
+app.use(passport.initialize());
+passport.use(passportStrategy);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
