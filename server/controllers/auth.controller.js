23319b29508e27c9b78cab6d8975eb17c737a362
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-16 22:56:13 +0300

    create user in auth controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -28,4 +28,17 @@ module.exports.login = async (req, res) => {
   }
 };
 
-module.exports.createUser = (req, res) => {};
+module.exports.createUser = async (req, res) => {
+  const candidate = await User.findOne({ login: req.body.login });
+  if (candidate) {
+    res.state(409).json({ message: "Такой логин уже занят" });
+  } else {
+    const salt = bcrypt.genSaltSync(10);
+    const user = new User({
+      login: req.body.login,
+      password: bcrypt.hashSync(req.body.password, salt)
+    });
+    await user.save();
+    res.status(201).json(user);
+  }
+};
