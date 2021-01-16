570fe439c2e1a99eaafe9c64610c289c05a6bb27
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-16 19:55:29 +0300

    controller, route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,3 @@
+module.exports.login = (req, res) => {};
+
+module.exports.createUser = (req, res) => {};
--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const keys = require("./keys");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth.routes");
+const keys = require("./keys");
 const app = express();
 
 mongoose
@@ -18,5 +19,7 @@ mongoose
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use('/api/auth',authRoutes)
 
 module.exports = app;
