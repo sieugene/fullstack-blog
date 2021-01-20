b07ad45d83e99d9382c1db21084b7632d30ab2a2
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-20 20:29:57 +0300

    post admin

--- pages/admin/post/_id.vue
+++ pages/admin/post/_id.vue
@@ -72,6 +72,9 @@ export default {
       }
     };
   },
+  mounted() {
+    this.controls.text = this.post.text;
+  },
   methods: {
     onSubmit() {
       this.$refs.form.validate(async valid => {
