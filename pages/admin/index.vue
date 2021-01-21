d96b0c06378d68019be2fc460c58cd85b04bcb2c
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-21 20:36:58 +0300

    analytics with data

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,61 +1,31 @@
 <template>
   <div>
     <h1>Аналитика по постам</h1>
-    <canvas height="50vh" ref="canvas"></canvas>
+    <AnalyticChart
+      title="Количество просмотров"
+      :labels="views.labels"
+      :data="views.data"
+    />
+    <AnalyticChart
+      title="Количество комментариев"
+      :labels="comments.labels"
+      :data="comments.data"
+    />
   </div>
 </template>
 <script>
-import { Bar } from "vue-chartjs";
+import AnalyticChart from "@/components/admin/AnalyticChart";
 
 export default {
+  components: {
+    AnalyticChart
+  },
   layout: "admin",
   middleware: ["admin-auth"],
-  extends: Bar,
   async asyncData({ store }) {
-    const analytics = await store.dispatch("post/getAnalytics");
-    return { analytics };
-  },
-  mounted() {
-    console.log(this.analytics);
+    const { views, comments } = await store.dispatch("post/getAnalytics");
 
-    const data = {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
-        {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)"
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)"
-          ],
-          borderWidth: 1
-        }
-      ]
-    };
-    const options = {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true
-            }
-          }
-        ]
-      }
-    };
-    this.renderChart(data, options);
+    return { views, comments };
   }
 };
 </script>
