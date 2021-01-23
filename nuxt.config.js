841bc57bbdfc941242d8bb5298aa7367fb2553b2
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-23 17:06:26 +0300

    pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,12 +25,13 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [],
+  buildModules: ["@nuxtjs/pwa"],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios"
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa"
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
